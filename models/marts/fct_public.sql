c3820982a5a18ae620a4ef5b7fd3905aab4f3cb4
Alex <naplle58@gmail.com>
2022-11-03 11:08:33 +0300

    Update dependencies

--- models/marts/fct_public.sql
+++ models/marts/fct_public.sql
@@ -10,14 +10,14 @@ WITH final AS (
 		AVG(re.passengers_count) AS "avg_passengers",
 		AVG(re.passengers_count * ro.price) AS "avg_money"
 	FROM
-		schedules s
-		FULL JOIN reports re ON s.id = re.id_schedule
-		JOIN routes ro ON s.id_route = ro.id
-		JOIN dim_route dr ON dr.old_id = ro.id
-		JOIN dim_group dg ON s.id_brigade = dg.number
-		JOIN dim_vehicle bv ON s.id_vehicle = bv.old_id
-		JOIN dim_vehicle_public dpv ON bv.id = dpv.id_vehicle
-		JOIN dim_time dt ON dt.day = date(started_at)
+		{{ source ("public", "schedules") }} s
+		FULL JOIN {{ source ("public", "reports") }} re ON s.id = re.id_schedule
+		JOIN {{ source ("public", "routes") }} ro ON s.id_route = ro.id
+		JOIN {{ ref ("dim_route") }} dr ON dr.old_id = ro.id
+		JOIN {{ ref ("dim_group") }} dg ON s.id_brigade = dg.number
+		JOIN {{ ref ("dim_vehicle") }} bv ON s.id_vehicle = bv.old_id
+		JOIN {{ ref ("dim_vehicle_public") }} dpv ON bv.id = dpv.id_vehicle
+		JOIN {{ ref ("dim_time") }} dt ON dt.day = date(started_at)
 	GROUP BY
 		dt.id,
 		dpv.id,
